5ef3cc3bf1eb847fc3b7e6366df1ec97456154fa
Sasha Okoronko <hvorost22@mail.ru>
2022-06-26 14:13:31 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+import ScrollTriggerProxy from "./components/ScrollTriggerProxy";
+import { AnimatePresence } from "framer-motion";
+
+import Loader from "./components/Loader";
+import Noise from "./components/Noise";
+import SoundBar from "./components/SoundBar";
+import Cursor from "./components/Cursor";
+import CursorManager from "./components/Cursor/CursorManager";
+import Header from "./components/Header";
+import MenuMobile from "./components/MenuMobile";
+import MenuManager from "./components/MenuMobile/MenuManager";
+import Modal from "./components/Modal";
+import ModalManager from "./components/Modal/ModalManager";
+
+import Intro from "./sections/Intro";
+import Skills from "./sections/Skills";
+import NewProjects from "./sections/NewProjects";
+import Projects from "./sections/Projects";
+import Applications from "./sections/Applications";
+import Footer from "./sections/Footer";
+
+import repair from "./assets/images/repair.jpg";
 
 function App() {
+  const containerRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 4500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Loader loading={loading} />
+      {!loading && (
+        <CursorManager>
+          <ModalManager>
+            <SoundBar />
+            <Noise />
+            <Cursor />
+            <LocomotiveScrollProvider
+              options={{
+                smooth: true,
+                inertia: 0.1,
+                smartphone: {
+                  smooth: true,
+                },
+                tablet: {
+                  smooth: true,
+                },
+              }}
+              watch={[]}
+              containerRef={containerRef}
+            >
+              <MenuManager>
+                <MenuMobile />
+                <ScrollTriggerProxy />
+                <AnimatePresence>
+                  <div
+                    className="App"
+                    id="app"
+                    data-scroll-container
+                    ref={containerRef}
+                  >
+                    <Header />
+                    <Intro />
+                    <main className="main">
+                      <Skills />
+                      <NewProjects />
+                      <Projects />
+                      <Applications />
+                    </main>
+                    <Footer />
+                  </div>
+                </AnimatePresence>
+              </MenuManager>
+            </LocomotiveScrollProvider>
+
+            <Modal>
+              <p>Сайт находится в режиме разработки!</p>
+              <img src={repair} alt="image" />
+            </Modal>
+          </ModalManager>
+        </CursorManager>
+      )}
+    </>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,110 @@
+import img1 from "../assets/images/project-1.jpg";
+import img2 from "../assets/images/project-2.jpg";
+import img3 from "../assets/images/project-3.jpg";
+
+export const projects = [
+  {
+    id: 1,
+    img: img1,
+    title: "movee",
+    type: "frontend",
+    year: 2022,
+    linkDemo: "https://sashaok47.github.io/WD-tasks",
+    linkCode: "https://github.com/SashaOK47/WD-tasks/",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "breaking bad",
+    type: "frontend",
+    year: 2022,
+    linkDemo: "https://sashaok47.github.io/Breaking-Bad/",
+    linkCode: "https://github.com/SashaOK47/Breaking-Bad",
+  },
+  {
+    id: 3,
+    img: img3,
+    title: "Personal Portfolio",
+    type: "frontend",
+    year: 2022,
+    linkDemo: "https://sashaok47.github.io/WD-tasks",
+    linkCode: "https://github.com/SashaOK47/WD-tasks/",
+  },
+  {
+    id: 4,
+    img: img1,
+    title: "movee",
+    type: "frontend",
+    year: 2022,
+    linkDemo: "https://sashaok47.github.io/WD-tasks",
+    linkCode: "https://github.com/SashaOK47/WD-tasks/",
+  },
+  {
+    id: 5,
+    img: img2,
+    title: "breaking bad",
+    type: "frontend",
+    year: 2022,
+    linkDemo: "https://sashaok47.github.io/Breaking-Bad/",
+    linkCode: "https://github.com/SashaOK47/Breaking-Bad",
+  },
+  {
+    id: 6,
+    img: img3,
+    title: "Personal Portfolio",
+    type: "frontend",
+    year: 2022,
+    linkDemo: "https://sashaok47.github.io/WD-tasks",
+    linkCode: "https://github.com/SashaOK47/WD-tasks/",
+  },
+];
+
+export const linksMenu = [
+  {
+    id: 1,
+    url: "#home",
+    title: "home",
+  },
+  {
+    id: 2,
+    url: "#skills",
+    title: "skills",
+  },
+  {
+    id: 3,
+    url: "#projects",
+    title: "projects",
+  },
+  {
+    id: 4,
+    url: "#applications",
+    title: "applications",
+  },
+  {
+    id: 5,
+    url: "#contacts",
+    title: "contacts",
+  },
+];
+export const socialLinks = [
+  {
+    id: 1,
+    title: "vkontacte",
+    url: "https://vk.com/",
+  },
+  {
+    id: 2,
+    title: "github",
+    url: "https://github.com/SashaOK47",
+  },
+  {
+    id: 3,
+    title: "facebook",
+    url: "https://www.facebook.com/",
+  },
+  {
+    id: 4,
+    title: "linkedin",
+    url: "https://www.linkedin.com/",
+  },
+];
